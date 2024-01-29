e15122eeff028da533afe79455df79321301a509
Nikita Malinkin <nik.malina20@gmail.com>
2024-01-29 18:20:26 +0300

    Solving tasks 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
-    public static void main(String[] args){
 
+    public static void main(String[] args){
+        String fullName = getFullName("Иванов", "Иван", "Иванович");
+        printFullNameToUpperCase(fullName);
+        replace("Иванов Семён Семёнович");
+    }
+
+    public static String getFullName(String lastName, String firstName, String middleName) {
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника: " + fullName);
+        return fullName;
+    }
+
+    public static void printFullNameToUpperCase(String fullName) {
+        String str = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + str);
+    }
+
+    public static void replace(String fullName) {
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
